59612bdfeec034e718a44e132d4ec70fdaf49fcd
Alcization <loc312004@gmail.com>
2025-03-26 07:16:20 +0700

    Update

--- firebase.config.js
+++ firebase.config.js
@@ -10,14 +10,16 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyDJRg6t4m34-JAlXF7Jkjr5FKWQ2OQnX7M",
-  authDomain: "lms-nm-232.firebaseapp.com",
-  projectId: "lms-nm-232",
-  storageBucket: "lms-nm-232.appspot.com",
-  messagingSenderId: "1003308906972",
-  appId: "1:1003308906972:web:a252b81e3be08beeb63719",
-  measurementId: "G-N2JYKQXL9H"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
